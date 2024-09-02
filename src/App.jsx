9359c4f2c03537c9b741d201592b5687e85a2020
yvonne <yvonnewong1107@gmail.com>
2024-09-03 00:16:31 +0100

    add react bootstrap and navlink corresponding pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
-import React from 'react';
-import { BrowserRouter  as Router, Route, Routes } from 'react-router-dom';
-import './App.css'
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import "./App.css";
 import Header from "./components/Header";
 import NavBar from "./components/NavBar";
 import Home from "./pages/Home";
@@ -8,23 +8,22 @@ import Menu from "./pages/Menu";
 import Contact from "./pages/Contact";
 import NoPage from "./pages/NoPage";
 
-
 function App() {
   return (
-      <Router>
-        {/* render header component */}
-        <Header />
-        {/* render navbar component */}
-        <NavBar />
-        {/* routes to render different pages */}
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="menu" element={<Menu />} />
-          <Route path="contact" element={<Contact />} />
-          <Route path="*" element={<NoPage />} />
-        </Routes>
-      </Router>
-  )
+    <Router>
+      {/* render header component */}
+      <Header />
+      {/* render navbar component */}
+      <NavBar />
+      {/* routes to render different pages */}
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/menu" element={<Menu />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/*" element={<NoPage />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
--- src/components/NavBar/index.jsx
+++ src/components/NavBar/index.jsx
@@ -1,10 +1,27 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
+// import Nav from 'react-bootstrap/Nav';
 
 function NavBar() {
-    return (
-        <h1>hi</h1>
-        // homepage, menu page, contact page
-    )
+  return (
+    <ul className="nav-items">
+      <li className="nav-item">
+        <NavLink to="/">
+          Home
+        </NavLink>
+      </li>
+      <li className="nav-item">
+        <NavLink to="/menu">
+          Menu
+        </NavLink>
+      </li>
+      <li className="nav-item">
+        <NavLink to="/contact">
+          Contact
+        </NavLink>
+      </li>
+    </ul>
+  );
 }
 
 export default NavBar;
