7d767528dc62a2f7ffbadbce647ce049b5403f2d
yvonne <yvonnewong1107@gmail.com>
2024-09-09 23:58:16 +0100

    add card component to contact page but still maintaining css grid format

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,21 +1,16 @@
 import React from "react";
+import Card from "../components/Card";
 
 function Contact() {
   return (
     <div className="main">
       <h1 className="page-title">Contact</h1>
       <p className="text-content">
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed neque
-        velit, lobortis ut magna varius, blandit rhoncus sem. Morbi lacinia nisi
-        ac dui fermentum, sed luctus urna tincidunt. Etiam ut feugiat ex. Cras
-        non risus mi. Curabitur mattis rutrum ipsum, ut aliquet urna imperdiet
-        ac. Sed nec nulla aliquam, bibendum odio eget, vestibulum tortor. Cras
-        rutrum ligula in tincidunt commodo. Morbi sit amet mollis orci, in
-        tristique ex. Donec nec ornare elit. Donec blandit est sed risus feugiat
-        porttitor. Vestibulum molestie hendrerit massa non consequat. Vestibulum
-        vitae lorem tortor. In elementum ultricies tempus. Interdum et malesuada
-        fames ac ante ipsum primis in faucibus.
+        <Card>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam est vitae eveniet tempore harum recusandae placeat ipsa dolore dicta deserunt molestiae necessitatibus, soluta provident natus magnam nobis facere. Voluptate, velit.
+        </Card>
       </p>
+      <p className="content">Hello</p>
     </div>
   );
 }
