433131856a0f28f5cfa419c50530c27e790ba356
yvonne <yvonnewong1107@gmail.com>
2024-09-18 22:59:42 +0100

    about content information

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -7,78 +7,10 @@ function About() {
     <div className="main">
       <h1 className="page-title">About</h1>
       <Card className="about-content">
-        Hi Lorem ipsum dolor sit amet, consectetur adipiscing elit. In vulputate
-        pulvinar enim sed venenatis. Duis est leo, sollicitudin sed diam vel,
-        tempus tincidunt ex. Nulla facilisi. Etiam aliquet lacus faucibus purus
-        commodo sollicitudin. Curabitur ac felis ut libero tincidunt pulvinar
-        vel sed est. Fusce lorem lectus, posuere quis sodales eu, sodales sit
-        amet neque. Aliquam non tellus diam. Cras vitae orci ante. Phasellus
-        vulputate eros et dapibus laoreet. Vivamus bibendum turpis at magna
-        aliquam, vitae elementum augue consequat. Ut luctus risus metus,
-        efficitur maximus metus lacinia faucibus. Donec congue magna ut risus
-        ultricies facilisis. Curabitur gravida accumsan molestie. Mauris
-        iaculis, orci in sollicitudin venenatis, ligula urna luctus lorem, quis
-        consequat turpis leo ac est. Fusce nec nulla urna. Suspendisse maximus
-        eu neque et lobortis. Nullam vulputate cursus libero. Nunc fringilla
-        enim turpis, ut mollis lacus auctor vitae. Sed sit amet egestas diam, a
-        tincidunt lectus. Nulla bibendum non velit vel mollis. Nulla eget ante
-        ac magna dictum pharetra. Quisque volutpat diam mauris, elementum
-        scelerisque felis tempus at. Praesent volutpat nibh quis pretium
-        placerat. Maecenas vel pulvinar orci. Suspendisse potenti. In hac
-        habitasse platea dictumst. Ut egestas, ex sit amet iaculis pulvinar,
-        diam tellus tincidunt risus, at ultrices turpis tortor nec dolor. Orci
-        varius natoque penatibus et magnis dis parturient montes, nascetur
-        ridiculus mus. Sed varius nisi vel risus varius pharetra. Nullam
-        tincidunt pharetra mi, tempor blandit nibh aliquam nec. Vestibulum ante
-        ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae;
-        Vivamus rhoncus risus sem, at eleifend enim eleifend id. Duis in laoreet
-        justo. Nam in lorem tempus, lacinia urna sed, venenatis risus. Duis
-        vehicula quam fermentum est varius sagittis. Sed vel nunc hendrerit,
-        consequat leo eu, lacinia justo. Maecenas sed ligula molestie, dapibus
-        lorem eu, efficitur justo. Nullam convallis fringilla magna, elementum
-        posuere augue accumsan a. In convallis feugiat sapien. Nullam ac feugiat
-        velit, vitae sollicitudin ante. Lorem ipsum dolor sit amet, consectetur
-        adipiscing elit. Cras aliquet elementum metus eu luctus. Curabitur
-        luctus at nisi at rhoncus. Aenean ac libero ac enim commodo consequat.
-        Proin laoreet urna a nisl scelerisque auctor. Mauris viverra id felis
-        vel lobortis. Hi Lorem ipsum dolor sit amet, consectetur adipiscing elit. In vulputate
-        pulvinar enim sed venenatis. Duis est leo, sollicitudin sed diam vel,
-        tempus tincidunt ex. Nulla facilisi. Etiam aliquet lacus faucibus purus
-        commodo sollicitudin. Curabitur ac felis ut libero tincidunt pulvinar
-        vel sed est. Fusce lorem lectus, posuere quis sodales eu, sodales sit
-        amet neque. Aliquam non tellus diam. Cras vitae orci ante. Phasellus
-        vulputate eros et dapibus laoreet. Vivamus bibendum turpis at magna
-        aliquam, vitae elementum augue consequat. Ut luctus risus metus,
-        efficitur maximus metus lacinia faucibus. Donec congue magna ut risus
-        ultricies facilisis. Curabitur gravida accumsan molestie. Mauris
-        iaculis, orci in sollicitudin venenatis, ligula urna luctus lorem, quis
-        consequat turpis leo ac est. Fusce nec nulla urna. Suspendisse maximus
-        eu neque et lobortis. Nullam vulputate cursus libero. Nunc fringilla
-        enim turpis, ut mollis lacus auctor vitae. Sed sit amet egestas diam, a
-        tincidunt lectus. Nulla bibendum non velit vel mollis. Nulla eget ante
-        ac magna dictum pharetra. Quisque volutpat diam mauris, elementum
-        scelerisque felis tempus at. Praesent volutpat nibh quis pretium
-        placerat. Maecenas vel pulvinar orci. Suspendisse potenti. In hac
-        habitasse platea dictumst. Ut egestas, ex sit amet iaculis pulvinar,
-        diam tellus tincidunt risus, at ultrices turpis tortor nec dolor. Orci
-        varius natoque penatibus et magnis dis parturient montes, nascetur
-        ridiculus mus. Sed varius nisi vel risus varius pharetra. Nullam
-        tincidunt pharetra mi, tempor blandit nibh aliquam nec. Vestibulum ante
-        ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae;
-        Vivamus rhoncus risus sem, at eleifend enim eleifend id. Duis in laoreet
-        justo. Nam in lorem tempus, lacinia urna sed, venenatis risus. Duis
-        vehicula quam fermentum est varius sagittis. Sed vel nunc hendrerit,
-        consequat leo eu, lacinia justo. Maecenas sed ligula molestie, dapibus
-        lorem eu, efficitur justo. Nullam convallis fringilla magna, elementum
-        posuere augue accumsan a. In convallis feugiat sapien. Nullam ac feugiat
-        velit, vitae sollicitudin ante. Lorem ipsum dolor sit amet, consectetur
-        adipiscing elit. Cras aliquet elementum metus eu luctus. Curabitur
-        luctus at nisi at rhoncus. Aenean ac libero ac enim commodo consequat.
-        Proin laoreet urna a nisl scelerisque auctor. Mauris viverra id felis
-        vel lobortis.
+        <span className="text-uppercase welcome">Welcome to WINNIE Chinese Takeaway</span><br /> Chinese and English hot and fresh meals to take away, based in Thorpe Hesley.
       </Card>
       <p className="content">
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Magnam maiores ea, sunt libero, eaque rem eligendi praesentium reprehenderit explicabo, nam labore iste delectus temporibus sapiente nihil commodi! Cum, eius explicabo!
+        Our takeaway offers a range of hot, fast, fresh Chinese and English dishes, for as long as 22 years.<br /> <br/>You can order in person or, to avoid waiting, over the phone. We are a collection-only takeaway and do not deliver. We accept cash and card payments. 
       </p>
     </div>
   );
