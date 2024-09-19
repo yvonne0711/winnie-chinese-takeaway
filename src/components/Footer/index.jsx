6a52e3990a211a9be42d13c04d851be7539d2bf8
yvonne <yvonnewong1107@gmail.com>
2024-09-19 19:07:30 +0100

    add last updated

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -2,10 +2,14 @@ import React from "react";
 import "./style.css";
 
 function Footer() {
+  // current date
+  const now = new Date().toLocaleDateString();
+
   return (
     <footer>
       <div>
         <span className="footer-disclaim">
+          Last updated: {now} <br/>
           We do not have any social media. This website is our only platform.
         </span>
         {/* grid layout */}
@@ -15,18 +19,10 @@ function Footer() {
             <div className="col-md-3 col-lg-4 col-xl-3 mb-3">
               <h6 className="text-uppercase fw-bold">Location</h6>
               <ul>
-                <li className="list-group-item">
-                  114 Thorpe Street
-                </li>
-                <li className="list-group-item">
-                  Thorpe Hesley
-                </li>
-                <li className="list-group-item">
-                  Rotherham
-                </li>
-                <li className="list-group-item">
-                  S61 2RP
-                </li>
+                <li className="list-group-item">114 Thorpe Street</li>
+                <li className="list-group-item">Thorpe Hesley</li>
+                <li className="list-group-item">Rotherham</li>
+                <li className="list-group-item">S61 2RP</li>
                 <li className="list-group-item">
                   <span>0114 245 7088</span>
                 </li>
@@ -34,16 +30,12 @@ function Footer() {
             </div>
 
             <div className="col-md-3 col-lg-4 col-xl-3 mb-3">
-              <h6 className="text-uppercase fw-bold">
-                Opening Hours
-                </h6>
+              <h6 className="text-uppercase fw-bold">Opening Hours</h6>
               <ul>
                 <li className="list-group-item">
                   Tuesday - Sunday: 17:30 - 21:00
                 </li>
-                <li className="list-group-item">
-                  Monday: Closed
-                  </li>
+                <li className="list-group-item">Monday: Closed</li>
               </ul>
             </div>
           </div>
