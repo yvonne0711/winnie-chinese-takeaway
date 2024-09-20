45ceb594faf6721c9df4aa98c5ba2a28e42758ad
yvonne <yvonnewong1107@gmail.com>
2024-09-20 19:05:02 +0100

    address in contact page above map

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -9,14 +9,24 @@ function Contact() {
       <h1 className="page-title">Contact</h1>
       <Card className="contact-card">
         Our takeaway is collection-only and we do not deliver. You can order in
-        person or, to avoid waiting, over the phone. Free on-street parking is available in front of the takeaway. We accept cash and card
+        person or, to avoid waiting, over the phone. We accept cash and card
         payments. <br /> <br />
         Opening Hours: <br />
         Tuesday-Sunday: 17:30 - 21:00 <br />
         Monday: Closed <br />
       </Card>
       <div className="content contact-content">
-        <MyMap/>
+        Free on-street parking is available in front of the takeaway. <br />
+        <ul>
+          <li className="list-group-item">114 Thorpe Street</li>
+          <li className="list-group-item">Thorpe Hesley</li>
+          <li className="list-group-item">Rotherham</li>
+          <li className="list-group-item">S61 2RP</li>
+          <li className="list-group-item">
+            <span>0114 245 7088</span>
+          </li>
+        </ul>
+        <MyMap />
       </div>
     </div>
   );
