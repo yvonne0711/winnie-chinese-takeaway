9f71761a4d0fddc6a6d70ec0bdcac25c7046c81c
yvonne <yvonnewong1107@gmail.com>
2024-09-20 18:29:51 +0100

    map location pan to takeaway longitude and latidude

--- src/components/Map/index.jsx
+++ src/components/Map/index.jsx
@@ -4,12 +4,12 @@ import './style.css';
 
 function MyMap() {
   return (
-    <MapContainer center={[51.505, -0.09]} zoom={13} className="yv-map">
+    <MapContainer center={[53.46138814267899, -1.433956895690216]} zoom={13} className="my-map">
       <TileLayer
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
       />
-      <Marker position={[51.505, -0.09]}>
+      <Marker position={[53.46138814267899, -1.433956895690216]}>
         <Popup>
           A pretty popup. <br /> Easily customizable.
         </Popup>
