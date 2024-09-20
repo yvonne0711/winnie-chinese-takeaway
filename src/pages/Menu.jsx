0826e9ce554e63da59e6a7ee044a3fd7c5ce5437
yvonne <yvonnewong1107@gmail.com>
2024-09-20 14:15:21 +0100

    allergy advice add

--- src/pages/Menu.jsx
+++ src/pages/Menu.jsx
@@ -11,12 +11,19 @@ function Menu() {
     <div className="main">
       <h1 className="page-title">Menu</h1>
       <Card className="menu-card">
-        We offer a range of traditional Chinese takeaway food. Popular choices are: our Mini Pancake Rolls, Barbecued Spare Ribs, our Black Bean Sauce dishes, our Crispy Chilli dishes, Special Curry, Special Fried Rice, Sweet & Sour Chicken, Beef with Mushrooms, Salt and Pepper Chips.
+        We offer a range of traditional Chinese takeaway food. Popular choices
+        are our Mini Pancake Rolls, Barbecued Spare Ribs, our Black Bean Sauce dishes, our Crispy Chilli dishes, Special Curry, Special Fried Rice, Sweet & Sour Chicken, Salt and Pepper Chips.
       </Card>
       <p className="content menu-content">
         <a href={menuPDF} target="_blank" rel="noopener noreferrer">
           Click to view Menu
         </a>
+        <br /><br/>
+        <span style={{fontStyle: "italic"}}>ALLERGY ADVICE:</span><br/> Some of our dishes may contain these 14 allergens as
+        follows: Cereals containing Gluten, Peanuts, Nuts, Milk, Soya, Mustard,
+        Lupin, Eggs, Fish, Crustaceans, Molluscs, Sesame Seeds, Celery, Sulphur
+        Dioxide. If you have any allergies to these products, please inform us
+        before ordering. We will do our best to advise you.
       </p>
     </div>
   );
